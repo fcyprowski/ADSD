d23051f07cbe5f0662bdc3b7bc071824868ff69d
Magdalena i Jakub <magdalena.lachowicz@gmail.com>
2017-06-24 15:23:52 +0200

    funkcja do wyciągania danych o postach

--- global.R
+++ global.R
@@ -1,1 +1,11 @@
 # tu powinny być wszystkie funkcje
+
+funkcja = function(url, token){
+  #tutaj wyciaga id z urla
+  id = gsub('http://facebook.com/', '', url)
+  id = gsub('/', '', id)
+  # tutaj robi getPage
+  getPage(id, token)
+  
+  
+}
