7dd40353b00b9bde1cbbc702824eb8d21248c35f
Maciej Baranowski <blackhship@gmail.com>
2017-06-24 14:58:02 +0200

    funkcja rankująca użytkowników

--- global.R
+++ global.R
@@ -1,1 +1,16 @@
-# tu powinny być wszystkie funkcje
+ranked_function = function(ex_data) {
+  x = ex_data %>%
+    group_by(Plant) %>%
+    summarise(total = n()) %>%
+    mutate(rank = dense_rank(desc(total))) %>%
+    filter(rank <= 10)
+  return(x)
+}
+
+
+
+
+
+
+
+
